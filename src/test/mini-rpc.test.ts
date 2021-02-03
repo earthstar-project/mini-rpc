e2340715bb1b1cda92915c6d982fe339aecb0a10
Cinnamon <cinnamon_bun_github@fastmail.com>
2021-02-03 15:11:14 -0800

    add tests

---
+++ src/test/mini-rpc.test.ts
@@ -0,0 +1,79 @@
+import t = require('tap');
+//t.runOnly = true;
+
+import {
+    logTest,
+    logTestLog,
+    logTestExpect,
+    logTestMark,
+    sleep,
+} from '../util';
+import {
+    evaluator,
+    makeProxy,
+    makeSimpleProxy,
+    myMethods
+} from '../mini-rpc';
+
+//================================================================================
+
+class Logger {
+    logs: string[] = [];
+    expected: string[] = [];
+    constructor() {
+        logTest('--- Logger begin ---');
+    }
+    log(msg: string) {
+        logTestLog(`--- ${msg} ---`);
+        this.logs.push(msg);
+    }
+    expect(msg: string) {
+        logTestExpect(`--- ${msg} ---`);
+        this.expected.push(msg);
+    }
+    marker(msg: string, verbose: boolean = true) {
+        if (verbose) {
+            logTestMark(`--- ${msg} ---`);
+        }
+        this.logs.push(msg);
+        this.expected.push(msg);
+    }
+    async sleep(ms: number) {
+        logTest(`===--- sleep... ---===`);
+        await sleep(ms);
+        logTest(`===--- ...sleep ---===`);
+        this.marker('sleep-' + ms, false);
+    }
+}
+
+//================================================================================
+
+t.test('simple proxy', async (t: any) => {
+    let proxy = makeSimpleProxy(myMethods);
+
+    t.deepEqual(await proxy.doubleSync(1), 2, 'doubleSync');
+    t.deepEqual(await proxy.doubleAsync(1), 2, 'doubleAsync');
+    t.deepEqual(await proxy.addSlowly(1, 2), 3, 'addSlowly');
+    try {
+        await proxy.divide(1, 0);
+        t.fail('divide by zero did not throw an error')
+    } catch (err) {
+        t.pass('divide by zero threw an error');
+    }
+    t.done();
+});
+
+t.test('evaluator proxy', async (t: any) => {
+    let proxy = makeProxy(myMethods, evaluator);
+
+    t.deepEqual(await proxy.doubleSync(1), 2, 'doubleSync');
+    t.deepEqual(await proxy.doubleAsync(1), 2, 'doubleAsync');
+    t.deepEqual(await proxy.addSlowly(1, 2), 3, 'addSlowly');
+    try {
+        await proxy.divide(1, 0);
+        t.fail('divide by zero did not throw an error')
+    } catch (err) {
+        t.pass('divide by zero threw an error');
+    }
+    t.done();
+});
--- src/util.ts
+++ src/util.ts
@@ -1,19 +1,19 @@
 import chalk = require('chalk');
 
 export let nop = (...args: any[]) => {};
-export let log = console.log;
+export let log = nop; //console.log;
 
-export let logMain =       (...args: any[]) => console.log(         chalk.black.bgWhite(     'main') + '        ', ...args);
-export let logHandler =    (...args: any[]) => console.log(' '   + chalk.black.bgCyanBright('handler') + '    ', ...args);
-export let logEvaluator =  (...args: any[]) => console.log('  ' + chalk.black.bgGreen(     'evaluator') + ' ', ...args);
+export let logMain =       (...args: any[]) => log(         chalk.black.bgWhite(     'main') + '        ', ...args);
+export let logHandler =    (...args: any[]) => log(' '   + chalk.black.bgCyanBright('handler') + '    ', ...args);
+export let logEvaluator =  (...args: any[]) => log('  ' + chalk.black.bgGreen(     'evaluator') + ' ', ...args);
 
-export let logTest =       (...args: any[]) => console.log(chalk.black.bgWhite(  'test       '), ...args);
-export let logTestMark =   (...args: any[]) => console.log(chalk.black.bgWhite(  'test ') + chalk.black.bgGray(       'mark  '), ...args);
-export let logTestLog =    (...args: any[]) => console.log(chalk.black.bgWhite(  'test ') + chalk.black.bgGreenBright('log   '), ...args);
-export let logTestExpect = (...args: any[]) => console.log(chalk.black.bgWhite(  'test ') + chalk.black.bgRedBright(  'expect'), ...args);
+export let logTest =       (...args: any[]) => log(chalk.black.bgWhite(  'test       '), ...args);
+export let logTestMark =   (...args: any[]) => log(chalk.black.bgWhite(  'test ') + chalk.black.bgGray(       'mark  '), ...args);
+export let logTestLog =    (...args: any[]) => log(chalk.black.bgWhite(  'test ') + chalk.black.bgGreenBright('log   '), ...args);
+export let logTestExpect = (...args: any[]) => log(chalk.black.bgWhite(  'test ') + chalk.black.bgRedBright(  'expect'), ...args);
 
-export let logHttpServer = (...args: any[]) => console.log(chalk.black.bgYellow('http server'), ...args);
-export let logHttpClient = (...args: any[]) => console.log(chalk.black.bgMagenta('http client'), ...args);
+export let logHttpServer = (...args: any[]) => log(chalk.black.bgYellow('http server'), ...args);
+export let logHttpClient = (...args: any[]) => log(chalk.black.bgMagenta('http client'), ...args);
 
 //================================================================================
 // HELPERS
