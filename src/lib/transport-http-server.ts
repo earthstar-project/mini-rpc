f325d8a73ebca87f250a182eb6e9d6317bd45b1a
Cinnamon <cinnamon_bun_github@fastmail.com>
2021-11-08 08:42:29 -0800

    SSE transport works for server-->client messages

--- src/lib/transport-http-server.ts
+++ src/lib/transport-http-server.ts
@@ -1,12 +1,11 @@
+import { transcode } from 'buffer';
 import express from 'express';
 import SSE from 'express-sse';
 //var SSE = require('express-sse');
 
 import { ITransport, Obj } from './types';
 
-let log = console.log;
-
-let app = express();
+let log = (...args: any[]) => console.log('SERVER  ', ...args);
 
 type Cb = (packet: Obj) => Promise<void>;
 type Thunk = () => void;
@@ -14,52 +13,78 @@ class TransportHttpServer implements ITransport {
     cbs: Set<Cb> = new Set();
     sse: any;
     app: any;
+    heartbeatInterval: any;
     constructor(app: any, public port: number) {
         log('constructor...');
-        log('set up listening for incoming POSTs');
+        log('constructor: set up listening for incoming POSTs');
         this.app = app;
         // listen for incoming POSTs
         app.post('/sync:' + this.port, (req: any, res: any) => {
-            log('got a POST');
+            log('!! express got a POST request');
             this.onReceive(JSON.parse(req.body));
         });
 
         // set up SSE for outgoing messages
-        log('set up SSE for outoing messages');
+        log('constructor set up SSE for outoing messages at /sync-sse');
         this.sse = new SSE();
-        app.get('/sync:' + this.port, this.sse.init);
+        app.get('/sync-sse:' + this.port, this.sse.init);
+
+        // turn on heartbeat
+        let heartbeatInterval = setInterval(() => this._sendHeartbeat(), 10000);
 
         log('...constructor is done.');
     }
     async send(packet: Obj): Promise<void> {
-        log('send: sending a message via sse:', JSON.stringify(packet));
-        this.sse.send(JSON.stringify(packet));
+        // TODO: no way of knowing if anyone is hearing us or not
+        log('send: sending a message via sse:', packet);
+        this.sse.send(packet);
         log('...send is done.');
+    }
+    async _sendHeartbeat(): Promise<void> {
+        log('(heartbeat)');
+        this.sse.send();
     }
     onReceive(cb: Cb): Thunk {
         this.cbs.add(cb);
         return () => this.cbs.delete(cb);
+    }
+    close(): void {
+        clearInterval(this.heartbeatInterval());
     }
 }
 
 //================================================================================
 let main = async () => {
     log('main (server)');
-    let PORT = 8123;
+    let PORT = 8008;
 
+    let app = express();
+    app.use(express.static('build'))
+
+    log('main: setting up transport');
     let transport = new TransportHttpServer(app, PORT);
-
-    log('main: sending hello world...');
-    await transport.send({hello: 'world', from: 'server'})
-    log('main: ...done sending.');
+    transport.onReceive(async (packet: Obj) => {
+        log('~~~ SERVER TRANSPORT GOT A MESSAGE:~~~', packet);
+    });
 
     log('main: setting up default / route in express');
     app.get('/', (req, res) => {
-        res.send('Hello world')
+        res.send('Hello world from server')
     })
 
-    log('main: listening...');
-    app.listen(PORT, () => console.log(`server is listening on https://localhost:${PORT}`));
+    setTimeout(() => {
+        log('main: listening...');
+        app.listen(PORT, () => console.log(`server is listening on http://localhost:${PORT}`));
+    }, 1);
+
+    let ii = 0;
+    setInterval(async () => {
+        log('----------------------');
+        log(`main: --> sending hello world packet ${ii} via sse...`);
+        await transport.send({hello: 'world', from: 'server', num: ii})
+        log('main: ...done sending.');
+        ii += 1
+    }, 5000);
 
     log('...main is done.');
 }
