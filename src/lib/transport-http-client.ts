5b1bfd3d8503edda47fea072b78c2a72537b8d06
Cinnamon <cinnamon_bun_github@fastmail.com>
2021-11-08 07:40:50 -0800

    trying to get browser-run working for tests

--- src/lib/transport-http-client.ts
+++ src/lib/transport-http-client.ts
@@ -1,22 +1,30 @@
 import { ITransport, Obj } from './types';
 
+let log = console.log;
+
 type Cb = (packet: Obj) => Promise<void>;
 type Thunk = () => void;
 class TransportHTTPClientSide implements ITransport {
     cbs: Set<Cb> = new Set();
     constructor(public url: string, public port: number) {
+        log('constructor');
+        log('constructor: set up SSE listening');
         // set up listening for SSE
         let evtSource = new EventSource('/sync:' + this.port);
         evtSource.onmessage = async (event) => {
+            log('sse onmessage handler');
             for (let cb of this.cbs) {
                 if (event.data.length > 0) {
                     await cb(event.data);
                 }
             }
         }
+        log('...constructor is done.');
     }
     async send(packet: Obj): Promise<void> {
+        log('send', JSON.stringify(packet));
         // send events as individual POSTs
+        log('send: doing a fetch POST...');
         let response = await fetch(this.url + ':' + this.port, {
             method: 'POST',
             headers: {
@@ -25,14 +33,17 @@ class TransportHTTPClientSide implements ITransport {
             },
             body: JSON.stringify(packet),
         });
+        log('send: ...fetch POST is done.');
         // if the request comes back as anything besides null, call the onReceive callback on it.
         // so there are two ways we can get messages: as responses here, and via SSE.
+        log('send: handling response...');
         if (response.text.length > 0) {
             let packetResponse = JSON.parse(await response.json());
             for (let cb of this.cbs) {
                 await cb(packetResponse);
             }
         }
+        log('...send is done.');
     }
     onReceive(cb: Cb): Thunk {
         this.cbs.add(cb);
@@ -42,13 +53,18 @@ class TransportHTTPClientSide implements ITransport {
 
 //================================================================================
 let main = async () => {
+    log('main (client)');
     let PORT = 8123
 
+    log('setting up transport on port ' + PORT);
     let transport = new TransportHTTPClientSide(`https://localhost`, PORT);
     transport.onReceive(async (packet: Obj) => {
+        log('transport.onReceive got a message', JSON.stringify(packet));
         console.log(`got ${JSON.stringify(packet)}`)
     });
 
+    log('transport: await send hello world...');
     await transport.send({hello: 'world', from: 'client'})
+    log('...main is done.');
 }
 main();
--- src/lib/transport-http-server.ts
+++ src/lib/transport-http-server.ts
@@ -4,6 +4,8 @@ import SSE from 'express-sse';
 
 import { ITransport, Obj } from './types';
 
+let log = console.log;
+
 let app = express();
 
 type Cb = (packet: Obj) => Promise<void>;
@@ -13,18 +15,26 @@ class TransportHttpServer implements ITransport {
     sse: any;
     app: any;
     constructor(app: any, public port: number) {
+        log('constructor...');
+        log('set up listening for incoming POSTs');
         this.app = app;
         // listen for incoming POSTs
         app.post('/sync:' + this.port, (req: any, res: any) => {
+            log('got a POST');
             this.onReceive(JSON.parse(req.body));
         });
 
         // set up SSE for outgoing messages
+        log('set up SSE for outoing messages');
         this.sse = new SSE();
         app.get('/sync:' + this.port, this.sse.init);
+
+        log('...constructor is done.');
     }
     async send(packet: Obj): Promise<void> {
+        log('send: sending a message via sse:', JSON.stringify(packet));
         this.sse.send(JSON.stringify(packet));
+        log('...send is done.');
     }
     onReceive(cb: Cb): Thunk {
         this.cbs.add(cb);
@@ -34,16 +44,23 @@ class TransportHttpServer implements ITransport {
 
 //================================================================================
 let main = async () => {
+    log('main (server)');
     let PORT = 8123;
 
     let transport = new TransportHttpServer(app, PORT);
 
+    log('main: sending hello world...');
+    await transport.send({hello: 'world', from: 'server'})
+    log('main: ...done sending.');
+
+    log('main: setting up default / route in express');
     app.get('/', (req, res) => {
         res.send('Hello world')
     })
 
-    app.listen(PORT, () => console.log(`serving on https://localhost:${PORT}`));
+    log('main: listening...');
+    app.listen(PORT, () => console.log(`server is listening on https://localhost:${PORT}`));
 
-    await transport.send({hello: 'world', from: 'server'})
+    log('...main is done.');
 }
 main();
